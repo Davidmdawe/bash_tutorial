e93bd53c89c448f7ead64a90630799ad59f1f040
Davidmdawe <mdawedavid@gmail.com>
2020-07-23 10:24:15 +0200

    Submitted

---
+++ arrays.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+fruits=( "Apple" "Orange" "Banana" "Sapota" )
+fruits[3]='Green Apple'
+for fruit in ${fruits[@]}
+do
+        echo "Fruit Name is $fruit"
+done
+
+echo "Number of Fruits in Bucket is" ${#fruits[@]}
+echo "All Fruits ${fruits[@]}"
